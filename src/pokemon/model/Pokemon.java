cf69c20f101369cd43ba77fe20d8a71e0e453b49
Amber Bullough <amber@bertbullough.com>
2017-12-14 09:24:14 -0700

    Add code

--- src/pokemon/model/Pokemon.java
+++ src/pokemon/model/Pokemon.java
@@ -1,6 +1,46 @@
 package pokemon.model;
 
-public class Pokemon 
+public abstract class Pokemon 
 {
 
+	
+	private int healthPoints;
+	private int attackPonts;
+	private double enchancementModifier;
+	private int number;
+	private String name;
+	private boolean canEvolve;
+	
+	public Pokemon(String name, int number)
+	{
+		this.name = name;
+		this.number = number;
+	}
+	public final String[] getPokemonTypes()
+	{
+		
+		Class<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		
+		
+		for(int index = 0; index < types.length; index++)
+		{
+			String currentInterface = types[index].getCanonicalName();
+			pokeTypes[index] = currentInterface;
+		}
+		return pokeTypes;
+		
+		
+		
+	}
+	public String toString()
+	{
+			String description = "Hi, I am a " + name + ", and my HP is " + healthPoints;
+			return description;
+	}
+	public String getPokemonInformation()
+	{
+		String info = "This pokemon is of type: " + this.getClass().getSimpleName();
+		return info;
+	}
 }
---
+++ src/pokemon/model/TissueBox.java
@@ -0,0 +1,22 @@
+package pokemon.model;
+
+public class TissueBox extends Pokemon implements Derptastic
+{
+	public TissueBox (int number, String name)
+	{
+		super(number, name);
+		
+	}
+	public int annoy(int level)
+	{
+		int annoyance = -913124;
+		
+		return annoyance;
+	}
+	public String sayDerpy()
+	{
+		return "Derp";
+				
+	}
+	
+}
